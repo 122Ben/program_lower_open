0ddfa1d38ab7481a4925ce104e61180ab5ce3ffe
122Ben <zhenyang2017@gmail.com>
2026-07-03 17:37:07 +0800

    Apply .gitignore, remove ignored files from tracking

--- Craftsman_Motor/segger_rtt_printf.d
+++
@@ -1,5 +0,0 @@
-..\craftsman_motor\segger_rtt_printf.o: ..\SEGGER_RTT_V782\RTT\SEGGER_RTT_printf.c
-..\craftsman_motor\segger_rtt_printf.o: ..\SEGGER_RTT_V782\RTT\SEGGER_RTT.h
-..\craftsman_motor\segger_rtt_printf.o: ..\SEGGER_RTT_V782\RTT\../Config/SEGGER_RTT_Conf.h
-..\craftsman_motor\segger_rtt_printf.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-..\craftsman_motor\segger_rtt_printf.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
